1016314a2e8d48d44d4539d19610bf548e47f24b
Martinaf <martinaferguson97@gmail.com>
2022-05-17 12:46:52 -0300

    pruebas

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 for (let i = 1; i <= 5; i++)
 do{
     let ingresarNombre = prompt("Ingresar nombre");
-    alert(`Turno  N°${i}  Nombre: ${ingresarNombre}`);
+    alert (`Turno  N°${i}  Nombre: ${ingresarNombre}`);
 }while(! (i<=5)){
     alert ('Ya no hay turnos disponibles, vuelva mañana')
 }
