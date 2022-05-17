a21a84bb26352589dcd3c4c9315a8486b791ae6c
Martinaf <martinaferguson97@gmail.com>
2022-05-17 19:02:57 -0300

    correcciones en main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-for (let i = 1; i <= 5; i++)
-do{
-    let ingresarNombre = prompt("Ingresar nombre");
+for (let i = 1; i <= 5; i++){
+    let ingresarNombre = prompt("Ingresar nombre:   ");
+    while(ingresarNombre.trim() ==""){
+        ingresarNombre = prompt ("Por favor ingresa un nombre válido:  ")        
+    }
     alert (`Turno  N°${i}  Nombre: ${ingresarNombre}`);
-}while(! (i<=5)){
+    }
     alert ('Ya no hay turnos disponibles, vuelva mañana')
-}
-
